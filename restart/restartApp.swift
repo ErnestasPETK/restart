c287d6dd61dc4cd913c2b480b0852a658b962c16
ErnestasPETK <ernestas.petkevicius53@gmail.com>
2023-03-01 10:00:18 +0200

    Initial Commit

---
+++ restart/restartApp.swift
@@ -0,0 +1,17 @@
+//
+//  restartApp.swift
+//  restart
+//
+//  Created by Ernestas Petkevičius on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct restartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
